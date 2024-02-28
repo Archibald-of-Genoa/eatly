d4b097218e0fb16273e987165bf544502dddab6e
Your Name <archibaldofgenoa@yandex.ru>
2024-02-29 01:11:24 +0300

    One Article stage 2

---
+++ src/components/OneArticle/OneArticle.styled.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const H3 = styled.h3`
+  font-family: "Poppins";
+  font-size: 45px;
+  font-weight: 600;
+  color: #252525;
+  margin-top: 78px;
+  margin-bottom: 32px;
+  `;
+
+export const P = styled.p`
+  font-family: "Poppins";
+  font-size: 22px;
+  font-weight: 400;
+  color: #252525;
+  margin-top: 60px;
+  margin-bottom: 90px;
+`;
+
+export const OutlinedButtonContainer = styled.button`
+    width: 186px;
+    height: 59px;
+`
--- src/components/OneArticle/OneArticle.tsx
+++ src/components/OneArticle/OneArticle.tsx
@@ -2,7 +2,8 @@ import { ContainerStyled } from "../Container";
 import { Header } from "../Header";
 import { Footer } from "../Footer";
 import UserContainer from "../UserContainer/UserContainer";
-
+import { H3, OutlinedButtonContainer, P } from "./OneArticle.styled";
+import { BUTTON } from "../Button";
 const OneArticle = () => {
   return (
     <>
@@ -11,7 +12,18 @@ const OneArticle = () => {
       </ContainerStyled>
 
       <ContainerStyled>
+        <H3>How To Order Food On eatly ?</H3>
         <UserContainer />
+        <P>
+          It wasn't quite yet time to panic. There was still time to salvage the
+          situation. At least that is what she was telling himself. The reality
+          was that it was time to panic and there wasn't time to salvage the
+          situation, but he continued to delude himself into believing there
+          was.
+        </P>
+        <OutlinedButtonContainer>
+          <BUTTON view="outlined">All Articles</BUTTON>
+        </OutlinedButtonContainer>
       </ContainerStyled>
 
       <Footer />
